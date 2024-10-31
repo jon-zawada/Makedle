2d15684a051884b6425d0e2e4a894d48e9da2464
jon-zawada <jonathan_zawada@yahoo.com>
2024-10-31 01:14:06 -0700

    getuserbyid and createuser

--- server/src/Controllers/UserController.ts
+++ server/src/Controllers/UserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { UserModel } from "../Models/User";
+import { UserModel } from "../models/user";
 import { Pool } from "pg";
 
 export class UserController {
@@ -16,7 +16,32 @@ export class UserController {
         res.status(200).json(users);
       })
       .catch((error) => {
-        res.status(500).json({ message: "Error retrieving users" });
+        res.status(500).json({ message: "Error retrieving users", error });
+      });
+  };
+
+  getUserById = (req: Request, res: Response) => {
+    const id = req.params.id;
+    this.userModel
+      .getUserById(id)
+      .then((user) => {
+        res.status(200).json(user);
+      })
+      .catch((error) => {
+        res.status(500).json({ message: "Error retrieving user", error });
+      });
+  };
+
+  createUser = (req: Request, res: Response) => {
+    console.log(req.body)
+    const { username, email, password } = req.body;
+    this.userModel
+      .createUser(username, email, password)
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch((error) => {
+        res.status(500).json({ message: "Error creating user", error });
       });
   };
 }
--- server/src/Models/User.ts
+++ server/src/Models/User.ts
@@ -19,4 +19,27 @@ export class UserModel {
     const result = await this.pool.query("SELECT * FROM users");
     return result.rows;
   }
+
+  async getUserById(id: string): Promise<User[]> {
+    const result = await this.pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    return result.rows;
+  }
+
+  async createUser(
+    username: string,
+    email: string,
+    hashed_password: string
+  ): Promise<User[]> {
+    //TODO logic to actually hash password
+    //TODO validate request fields above and
+    //TODO empty fields/null checks
+    //TODO check if unique email
+    const result = await this.pool.query(
+      "INSERT INTO users (username, email, hashed_password) VALUES ($1, $2, $3) RETURNING *",
+      [username, email, hashed_password]
+    );
+    return result.rows;
+  }
 }
