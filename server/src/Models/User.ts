0ef39e40decd7808e8202516809aaa0baf9200a0
jon-zawada <jonathan_zawada@yahoo.com>
2024-11-02 21:58:08 -0700

    testing with some format changes

--- server/src/Models/User.ts
+++ server/src/Models/User.ts
@@ -53,7 +53,10 @@ export class UserModel {
     return result.rows[0] || null;
   };
 
-  updateRefreshToken = async (id: number, refreshToken: string | null): Promise<void> => {
+  updateRefreshToken = async (
+    id: number,
+    refreshToken: string | null
+  ): Promise<void> => {
     const query = "UPDATE users SET refresh_token = $1 WHERE id = $2";
     await this.pool.query(query, [refreshToken, id]);
   };
--- server/src/middleware/authenticateJWT.ts
+++ server/src/middleware/authenticateJWT.ts
@@ -1,16 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-const authenticateJWT = (req: Request, res: Response, next: NextFunction): void | Response => {
-  const authHeader = req.header('Authorization');
-  const token = authHeader?.startsWith('Bearer ') ? authHeader.split(' ')[1] : authHeader;
+const authenticateJWT = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void | Response => {
+  const authHeader = req.header("Authorization");
+  const token = authHeader?.startsWith("Bearer ")
+    ? authHeader.split(" ")[1]
+    : authHeader;
 
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET!, (err: any, user: any) => {
       if (err) {
         return res.sendStatus(403);
       }
-  
+
       req.user = user;
       next();
     });
