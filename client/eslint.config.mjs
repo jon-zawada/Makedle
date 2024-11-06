74833b22cefc6b0e252cf6ccca408100d61722e5
jon-zawada <jonathan_zawada@yahoo.com>
2024-11-05 19:44:06 -0800

    working, had to remove eslint ignore

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -6,9 +6,21 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: ["dist/**/*", "node_modules/**/*"]
+  },
+  {
+    languageOptions: { globals: {...globals.browser, ...globals.node} }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
 ];
