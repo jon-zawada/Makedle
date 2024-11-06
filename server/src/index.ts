3201867200616b3912ec81869aa40f3fb9746ccb
jon-zawada <jonathan_zawada@yahoo.com>
2024-11-05 18:12:25 -0800

    server client bundle

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import userRoutes from "./Routes/userRoutes";
 import gameRoutes from "./Routes/gameRoutes";
+
+const CLIENT_DIR = path.join(__dirname, "..", "..", "client", "dist");
 
 dotenv.config();
 
@@ -11,8 +14,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(CLIENT_DIR));
+
 app.use("/api", userRoutes);
 app.use("/api", gameRoutes);
+
+//must be last route
+app.get("*", (req, res) => {
+  res.sendFile(CLIENT_DIR);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
