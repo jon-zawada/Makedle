0d67e4604e068224dd97f2bd0e9ff9b6b5445953
jon-zawada <jonathan_zawada@yahoo.com>
2024-11-08 00:27:29 -0800

    hello world for tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { Button, Typography } from '@mui/material';
 
 const App: React.FC = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <Typography variant="h4" gutterBottom>
-        Hello, Makedle!
-      </Typography>
-      <Button variant="contained" color="primary">
-        I dont do anything
-      </Button>
+    <div className='bg-red-500 text-center'>
+      Hello world
     </div>
   );
 };
