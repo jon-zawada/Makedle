b5cefd072d93dbb6068eaf9f266de9faa9505176
jon-zawada <jonathan_zawada@yahoo.com>
2024-11-09 21:27:41 -0800

    grid design layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,22 @@
-import React, { useEffect } from "react";
+import React from "react";
 import PageHeader from "./components/PageHeader";
 import Button from "./components/common/Button";
-import httpService from "./api/httpService";
 
 const App = () => {
-
-  //delete this later
-  useEffect(() => {
-    httpService.get("/games")
-      .then(res => {
-        console.log(res.data);        
-      })
-  }, []);
-
   return (
-    <div className="max-h-screen flex flex-col">
+    <div className="grid grid-rows-[auto_1fr_auto] grid-cols-1 md:grid-cols-[250px_1fr] gap-4 max-h-screen">
       <PageHeader />
-      <div className="flex gap-1 justify-center">
-        <Button>
-          Play a game
-        </Button> 
-        <Button>
-          Make a game
-        </Button> 
-      </div>
+      <aside className="bg-gray-300 p-4">SIDEBAR</aside>
+      <main className="flex gap-1 justify-center h-[75vh] bg-green-50">
+        <div className="flex flex-col justify-center items-center">
+          <div>MAIN CONTENT</div>
+          <div className="flex gap-1">
+            <Button>Play a game</Button>
+            <Button>Make a game</Button>
+          </div>
+        </div>
+      </main>
+      <footer className="bg-green-100 p-4 col-span-2 text-center">FOOTER</footer>
     </div>
   );
 };
