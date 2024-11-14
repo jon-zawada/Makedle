872cb4a88ac19e135ff0a7ceb280ec0ade92a580
jon-zawada <jonathan_zawada@yahoo.com>
2024-11-14 01:17:41 -0800

    fail tests on purpose

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,37 +1,5 @@
 import React from "react";
-import { VariantProps, cva } from "class-variance-authority";
-import { ComponentProps } from "react";
-import { twMerge } from "tailwind-merge";
 
-const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
-  variants: {
-    variant: {
-      default: ["bg-secondary", "hover:bg-secondary-hover"],
-      ghost: ["hover:bg-gray-100"],
-    },
-    size: {
-      default: ["rounded", "p-2"],
-      icon: ["rounded-full", "w-10", "h-10", "flex", "items-center", "p-2.5"],
-    },
-  },
-  defaultVariants: {
-    variant: "default",
-    size: "default",
-  },
-});
-
-type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
-
-export default function Button({
-  variant,
-  size,
-  className,
-  ...props
-}: ButtonProps) {
-  return (
-    <button
-      {...props}
-      className={twMerge(buttonStyles({ variant, size, className }), className)}
-    />
-  );
+export default function Button({ ...props }) {
+  return <button {...props} />;
 }
