511dc0256e8c7152add9fd0649c67d5695994f5b
jon-zawada <jonathan_zawada@yahoo.com>
2024-11-14 01:38:16 -0800

    tests should be required

--- client/src/api/httpService.ts
+++ client/src/api/httpService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const httpService = axios.create({
   baseURL: "http://localhost:3000/api",
-  timeout: 10000,
+  timeout: 10001,
   headers: {
     "x-makedle-header": "foobar",
     // Authorization: `Bearer ${localStorage.getItem('token')}`,  // Add JWT token - setting JWT not yet implemented
