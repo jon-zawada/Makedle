fbacadd5c92223f965d13dfbad8e23ba5faebe7b
jon-zawada <jonathan_zawada@yahoo.com>
2024-11-19 00:41:30 -0800

    sample test and user tests

---
+++ server/jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov"],
+  collectCoverageFrom: [
+    "src/**/*.{js,ts}",
+    "!src/**/*.test.{js,ts}",
+  ],
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", {}],
+  },
+  moduleDirectories: ["node_modules", "src"],
+  testMatch: ["**/*.test.ts"],
+  clearMocks: true,
+};
