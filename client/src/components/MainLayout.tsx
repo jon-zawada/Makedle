b4cfb60108f4dfe00fc269535338d3a3450b78ec
jon-zawada <jonathan_zawada@yahoo.com>
2024-11-21 01:23:03 -0800

    hide sidebar if not logged in

--- client/src/components/MainLayout.tsx
+++ client/src/components/MainLayout.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import PageHeader from "../components/PageHeader";
 import Sidebar from "../components/Sidebar";
+import { useAuth } from "../context/AuthProvider";
 
 const MainLayout = () => {
+  const {appUser} = useAuth();
+
   return (
     <div className="grid grid-cols-[250px_1fr] gap-4 max-h-screen">
       <PageHeader />
-      <Sidebar />
-      <main className="h-[75vh] bg-green-50">
+      {appUser && <Sidebar />}
+      <main className={`h-[75vh] bg-green-50 ${!appUser && "col-span-2"}`}>
         <Outlet />
       </main>
       <footer className="bg-green-100 p-4 col-span-2 text-center">
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -7,15 +7,13 @@ const HomePage = () => {
   // TODO delete
   const test = () => {
     httpService.get("/users");
-  }
+  };
   return (
-    <div>
-      <div className="flex flex-col justify-center items-center">
-        <div>MAIN CONTENT</div>
-        <div className="flex gap-1">
-          <Button onClick={test}>Play a game</Button>
-          <Button>Make a game</Button>
-        </div>
+    <div className="flex flex-col justify-center items-center">
+      <div>MAIN CONTENT</div>
+      <div className="flex gap-1">
+        <Button onClick={test}>Play a game</Button>
+        <Button>Make a game</Button>
       </div>
     </div>
   );
