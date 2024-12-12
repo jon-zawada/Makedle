cf564d6f625f5aee14c8d187969f98e51dc2dcfa
Jon Zawada <jonathan_zawada@yahoo.com>
2024-12-11 21:40:32 -0800

    empty state

---
+++ client/src/components/common/EmptyState.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Ban } from 'lucide-react'
+
+export default function EmptyState() {
+  return (
+    <div>
+      <Ban />
+    </div>
+  )
+}
--- client/src/pages/games/GamesPage.tsx
+++ client/src/pages/games/GamesPage.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import _isEmpty from "lodash/isEmpty";
 import useHttpService from "../../api/useHttpService";
 import { Game } from "../../types/types";
 import GameItem from "./GameItem";
 import PageLayout from "../../components/common/PageLayout";
 import toast from "react-hot-toast";
+import EmptyState from "../../components/common/EmptyState";
 
 export default function GamesPage() {
   const [games, setGames] = useState([]);
@@ -19,7 +21,7 @@ export default function GamesPage() {
       setLoading(true);
       const response = await httpService.get("/games");
       const { games, totalCount } = response.data;
-      console.log(totalCount) //TODO pagination on client
+      console.log(totalCount); //TODO pagination on client
       setGames(games);
     } catch {
       toast.error("Issue loading games, try again later");
@@ -30,11 +32,15 @@ export default function GamesPage() {
 
   return (
     <PageLayout title="Games" loading={loading}>
-      <div className="grid gap-4  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {games.map((game: Game) => (
-          <GameItem key={game.id} game={game} />
-        ))}
-      </div>
+      {_isEmpty(games) ? (
+        <EmptyState />
+      ) : (
+        <div className="grid gap-4  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {games.map((game: Game) => (
+            <GameItem key={game.id} game={game} />
+          ))}
+        </div>
+      )}
     </PageLayout>
   );
 }
