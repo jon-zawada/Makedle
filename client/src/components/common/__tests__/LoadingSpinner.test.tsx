f123a7cc129faa52e010555d4095b51e2b86e0c0
jon-zawada <jonathan_zawada@yahoo.com>
2024-12-12 22:33:58 -0800

    loading spinner test

---
+++ client/src/components/common/__tests__/LoadingSpinner.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import LoadingSpinner from "../LoadingSpinner";
+import "@testing-library/jest-dom";
+
+describe("LoadingSpinner component", () => {
+  it("renders without crashing", () => {
+    render(<LoadingSpinner />);
+    expect(screen.getByText(/loading.../i)).toBeInTheDocument();
+  });
+
+  it("has the correct styles for the spinner", () => {
+    const { container } = render(<LoadingSpinner />);
+    const spinner = container.querySelector("div.w-16.h-16");
+    expect(spinner).toHaveClass("border-4");
+    expect(spinner).toHaveClass("border-green-500");
+    expect(spinner).toHaveClass("animate-spin");
+  });
+});
