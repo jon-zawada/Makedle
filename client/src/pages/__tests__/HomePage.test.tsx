35627c331f03b4955f2f155bd2112e8725ae16b8
jon-zawada <jonathan_zawada@yahoo.com>
2024-12-18 23:34:28 -0800

    update home jest test

--- client/src/pages/__tests__/HomePage.test.tsx
+++ client/src/pages/__tests__/HomePage.test.tsx
@@ -3,16 +3,25 @@ import { render, screen } from "@testing-library/react";
 import HomePage from "../HomePage";
 import AuthProvider from "../../context/AuthProvider";
 import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
 
 describe("HomePage", () => {
   test("renders the home page", () => {
     render(
-      <AuthProvider>
-        <HomePage />
-      </AuthProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <HomePage />
+        </AuthProvider>
+      </BrowserRouter>
     );
-    expect(screen.getByText("Home")).toBeInTheDocument();
-    expect(screen.getAllByRole("button", { name: "Play a game" }));
-    expect(screen.getAllByRole("button", { name: "Make a game" }));
+    expect(screen.getByText("Welcome to Makedle")).toBeInTheDocument();
+    
+    const createLink = screen.getByText("Create", { selector: "span" });
+    expect(createLink).toBeInTheDocument();
+    expect(createLink.closest("a")).toHaveAttribute("href", "/create");
+
+    const playLink = screen.getByText("Play", { selector: "span" });
+    expect(playLink).toBeInTheDocument();
+    expect(playLink.closest("a")).toHaveAttribute("href", "/games");
   });
 });
