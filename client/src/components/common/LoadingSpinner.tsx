3ad3e2ecc9f65c01c0edc896e447ba4abf52ac85
jon-zawada <jonathan_zawada@yahoo.com>
2025-01-25 14:32:16 -0800

    loading spinner

--- client/src/components/common/LoadingSpinner.tsx
+++ client/src/components/common/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function LoadingSpinner() {
   return (
     <div className="flex flex-col gap-2 items-center justify-center h-full">
-      <div className="w-16 h-16 border-4 border-green-500 border-t-transparent rounded-full animate-spin will-change-transform"></div>
+      <div className="w-16 h-16 border-4 border-gray-200 border-t-green-500 rounded-full animate-spin will-change-transform"></div>
       <div>Loading...</div>
     </div>
   );
