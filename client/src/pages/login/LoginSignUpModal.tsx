830efd296ccf30a8e4f4ccb55d1171aa1af56292
jon-zawada <jonathan_zawada@yahoo.com>
2025-05-12 02:04:41 -0700

    refactor: for dynamic initial views of login modal

--- client/src/pages/login/LoginSignUpModal.tsx
+++ client/src/pages/login/LoginSignUpModal.tsx
@@ -1,33 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "@mui/material/Modal";
 import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm";
 
-interface LoginSignUpModal {
+interface LoginSignUpModalProps {
   isOpen: boolean;
   handleClose: () => void;
+  initLogin?: boolean; // Optional, defaults to true
 }
 
 export default function LoginSignUpModal({
   isOpen,
   handleClose,
-}: LoginSignUpModal) {
-  const [showLogin, setShowLogin] = useState(true);
+  initLogin = true,
+}: LoginSignUpModalProps) {
+  const [showLogin, setShowLogin] = useState(initLogin);
+
+  useEffect(() => {
+    if (isOpen) {
+      setShowLogin(initLogin);
+    }
+  }, [isOpen, initLogin]);
 
   return (
-    <div>
-      <Modal
-        open={isOpen}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        {showLogin ? (
-          <LoginForm showSignUp={() => setShowLogin(false)} />
-        ) : (
-          <SignUpForm showLogin={() => setShowLogin(true)} />
-        )}
-      </Modal>
-    </div>
+    <Modal
+      open={isOpen}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      {showLogin ? (
+        <LoginForm showSignUp={() => setShowLogin(false)} />
+      ) : (
+        <SignUpForm showLogin={() => setShowLogin(true)} />
+      )}
+    </Modal>
   );
 }
