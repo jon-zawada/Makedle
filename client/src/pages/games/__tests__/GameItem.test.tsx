05840c367184f32a40742006d11902d31e2acc71
jon-zawada <jonathan_zawada@yahoo.com>
2025-06-22 02:14:27 -0700

    fix unit tests

--- client/src/pages/games/__tests__/GameItem.test.tsx
+++ client/src/pages/games/__tests__/GameItem.test.tsx
@@ -28,10 +28,9 @@ describe("GameItem Component", () => {
     (useNavigate as jest.Mock).mockReturnValue(mockNavigate);
   });
 
-  it("renders the game name and image", () => {
+  it("renders the game image", () => {
     render(<GameItem game={mockGame} />);
 
-    expect(screen.getByText(mockGame.name)).toBeInTheDocument();
     expect(screen.getByRole("img")).toHaveAttribute("src", mockGame.image);
   });
 
@@ -51,28 +50,20 @@ describe("GameItem Component", () => {
     render(<GameItem game={gameWithoutImage} />);
 
     const image = screen.getByRole("img");
-    expect(image).toHaveAttribute("alt", "Game Without Image image");
+    expect(image).toHaveAttribute("alt", "Game Without Image");
     expect(image).toHaveAttribute("src", "");
   });
 
   it("navigates to the correct game page on click", () => {
-    render(<GameItem game={mockGame} />);
+    const {debug} = render(<GameItem game={mockGame} />);
 
-    const gameItem = screen.getByText(mockGame.name);
+    debug();
+
+    const gameItem = screen.getByRole('img');
     fireEvent.click(gameItem);
 
     expect(mockNavigate).toHaveBeenCalledWith(`/games/${mockGame.id}`, {
       state: { gameData: mockGame },
     });
-  });
-
-  it("applies the correct hover styles and animations", () => {
-    render(<GameItem game={mockGame} />);
-
-    const gameItem = screen.getByText(mockGame.name);
-    fireEvent.mouseOver(gameItem);
-    const gameName = screen.getByText(mockGame.name);
-    expect(gameName).toHaveClass("hover:underline");
-    expect(gameName).toHaveClass("hover:text-green-500");
   });
 });
